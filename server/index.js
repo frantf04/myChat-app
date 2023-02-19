22bdf1e347cc071c0a8ef4ee8d40b6703052bcb8
Johan Olmos <frantf04@gmail.com>
2023-02-18 23:16:04 -0400

    nothing

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import cors from "cors";
 
 // Initializations
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app)
 const io = new SocketServer(server, {
   // cors: {
   //   origin: "http://localhost:3000",
